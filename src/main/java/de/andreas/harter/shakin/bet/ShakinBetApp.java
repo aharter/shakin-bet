f5cc823e01ba79fd73035190e4ce3f18f757756c
Andreas Harter <git@andreas-harter.de>
2016-11-21 20:21:30 +0100

    Add initial spring boot context

---
+++ src/main/java/de/andreas/harter/shakin/bet/ShakinBetApp.java
@@ -0,0 +1,13 @@
+package de.andreas.harter.shakin.bet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShakinBetApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShakinBetApp.class, args);
+    }
+
+}
